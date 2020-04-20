73fedf84bc92d7542d0b7a415621557fa7801e9c
KimGeeWon <kjw403300@gmail.com>
2020-04-20 13:47:43 +0900

    [ADD] findViewById
    findViewById 실습

--- HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
+++ HelloWorld/app/src/main/java/com/example/helloworld/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView.setText("Simple Calculator!!");
     }
 }
